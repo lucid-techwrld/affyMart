5e0c16dd84f72f82f3738e07bbc30e613da855ea
lucid-techwrld <davisredtiger@gmail.com>
2024-08-25 22:55:51 +0100

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSidebar() {
+  const show = document.querySelector('.navBar');
+  show.style.display ='flex'
+}
+
+function closeSidebar() {
+  const show = document.querySelector('.navBar');
+  show.style.display ='none'
+}
