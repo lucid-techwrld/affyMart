7c7836e4847074b6eb1be640ff745fca177387e9
lucid-techwrld <davisredtiger@gmail.com>
2024-08-29 14:31:18 +0100

    update

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ function closeSidebar() {
   const show = document.querySelector('.navBar');
   show.style.display ='none'
 }
+
+// JavaScript code to enable auto-scroll
+const slider = document.querySelector('.slider');
+let currentIndex = 0;
+const totalSlides = slider.children.length;
+
+function autoScroll() {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  slider.scrollTo({
+    left: slider.offsetWidth * currentIndex,
+    behavior: 'smooth'
+  });
+}
+
+// Set the interval to scroll every 3 seconds
+setInterval(autoScroll, 3000);
+
+let cartQuantity = 0;
+
+function addCart() {
+  const cartDisplay = document.getElementById('cartId');
+  cartQuantity = cartQuantity + 1;
+  
+  cartDisplay.textContent = cartQuantity
+  console.log(cartQuantity)
+}
+
