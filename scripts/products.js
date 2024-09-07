b5ab57c38a776b7713e1392e81e7750624538f2a
lucid-techwrld <davisredtiger@gmail.com>
2024-09-07 11:08:15 +0100

    update

--- scripts/products.js
+++ scripts/products.js
@@ -8,15 +8,40 @@ products.forEach((product, index) => {
       <div class="product-info">
         <div class="product-name">
           <p id="product-name">${product.name}</p>
-          <p id="description">${product.desc}</p>
+          <p class="add-cart-message"></p>
         </div>
         <div class="price-cart">
           <p id="price">$${(product.priceCents / 100).toFixed(2)} </p>
-          <button onclick="addCart(${index})">+</button>
+          <button class= "add-cart-button" data-product-id="${product.id}">+</button>
         </div>
       </div>
     </div>
   </div>
   `;
   document.querySelector('.products-box').innerHTML += html;
+});
+
+document.querySelectorAll('.add-cart-button').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+    let matchingItem;
+    
+    cart.forEach((item) => {
+      
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+    });
+        if (matchingItem) {
+          matchingItem.quantity += 1;
+        }
+        else {
+          cart.push({
+            productId: productId,
+            quantity: 1
+          });
+        }
+    
+    console.log(cart);
+  });
 });
